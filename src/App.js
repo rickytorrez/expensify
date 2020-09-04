c07651a580fbef7e3d8eecf4c13983946b0f4c23
rickytorrez <torrez.ricky@ymail.com>
2020-09-04 13:42:48 -0400

    test distpach and filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
 import './styles/styles.scss';
 
 const store = configureStore();
-console.log(store.getState());
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 100 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 50 }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 function App() {
   return <AppRouter />;
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,22 +1,25 @@
 // get visible expenses
 export default (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses
-    .filter((expense) => {
-      const startDateMatch =
-        typeof startDate !== 'number' || expense.createdAt >= startDate; // only if start date is a number, we want to filter expenses
-      const endDateMatch =
-        typeof endDate !== 'number' || expense.createdAt <= endDate;
-      const textMatch = expense.description
-        .toLowerCase()
-        .includes(text.toLowerCase());
+  return (
+    expenses
+      .filter((expense) => {
+        const startDateMatch =
+          typeof startDate !== 'number' || expense.createdAt >= startDate; // only if start date is a number, we want to filter expenses
+        const endDateMatch =
+          typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description
+          .toLowerCase()
+          .includes(text.toLowerCase());
 
-      return startDateMatch && endDateMatch && textMatch;
-    })
-    .sort((a, b) => {
-      if (sortBy === 'date') {
-        return a.createdAt < b.createdAt ? 1 : -1;
-      } else if ((sortBy = 'amount')) {
-        return a.amount < b.amount ? 1 : -1;
-      }
-    });
+        return startDateMatch && endDateMatch && textMatch;
+      })
+      // eslint-disable-next-line
+      .sort((a, b) => {
+        if (sortBy === 'date') {
+          return a.createdAt < b.createdAt ? 1 : -1;
+        } else if ((sortBy = 'amount')) {
+          return a.amount < b.amount ? 1 : -1;
+        }
+      })
+  );
 };
