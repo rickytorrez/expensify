bb2f7ec5ca631877382635befd28464deadd8aa2
rickytorrez <torrez.ricky@ymail.com>
2020-09-08 12:30:06 -0400

    child components, selector use

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ store.dispatch(addExpense({ description: 'Gas bill', amount: 50 }));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'));
+  store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
