8ae85e02614472508359c707086a8816043970b1
rickytorrez <torrez.ricky@ymail.com>
2020-09-08 15:45:13 -0400

    filtering by amount or date via select

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 import './styles/styles.scss';
@@ -13,7 +12,6 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 195000 }));
-store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
