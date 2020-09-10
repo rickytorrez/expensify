dad55fe57952da37817c25e86212c65925400529
rickytorrez <torrez.ricky@ymail.com>
2020-09-10 14:20:45 -0400

    filtering by date, moment, react-dates

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,11 @@
+import moment from 'moment';
+
 // default state value
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined,
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month'),
 };
 
 // filter reducer
