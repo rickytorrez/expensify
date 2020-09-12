00c01e7e1100bf64fbd28515ed7dee1671f0df31
rickytorrez <torrez.ricky@ymail.com>
2020-09-12 11:41:03 -0400

    jest import error

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,13 @@
+import {
+  addExpense,
+  editExpense,
+  removeExpense,
+} from '../../actions/expenses.js';
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toBe({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc',
+  });
+});
