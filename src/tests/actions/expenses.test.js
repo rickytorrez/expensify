1eba5bfb4fe5c19c77ae704cf1ff3bf564894b16
rickytorrez <torrez.ricky@ymail.com>
2020-09-14 12:32:40 -0400

    jest, babel testing set up

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,7 +6,7 @@ import {
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
-  expect(action).toBe({
+  expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc',
   });
