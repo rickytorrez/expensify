bba9bb958b24c87d23b757322d0a765a22c65a46
rickytorrez <torrez.ricky@ymail.com>
2020-09-21 11:52:33 -0400

    notes

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,6 +6,6 @@ import ExpenseListItem from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
 
 test('should render ExpenseListItem correctly', () => {
-  const wrapper = shallow(<ExpenseListItem {...expenses[1]} />);
+  const wrapper = shallow(<ExpenseListItem {...expenses[1]} />); // spread the expense at index[1] rather than writing all properties in
   expect(toJSON(wrapper)).toMatchSnapshot();
 });
